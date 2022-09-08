75db673a4fc7956aa7cdeedc16fc31cb64bce880
Max Zinovik <zinovik@gmail.com>
2022-09-08 20:09:42 +0400

    add uzbekistan categories

--- tools/find-missing-items.js
+++ tools/find-missing-items.js
@@ -6,13 +6,20 @@ images.forEach((image) => {
   const imageUrl = imageUrls.find((imageUrl) =>
     imageUrl.includes(image.filename)
   );
-  if (!imageUrl) console.log(`Missing image url for: ${image.filename}`);
+  if (!imageUrl) console.log(`Missing image url for image: ${image.filename}`);
 
   const section = sections.find((section) => section.path === image.path);
-  if (!section) console.log(`Missing section for path: ${image.path}`);
+  if (!section)
+    console.log(`Missing section for the image path: ${image.path}`);
 });
 
 imageUrls.forEach((imageUrl) => {
   const image = images.find((image) => imageUrl.includes(image.filename));
-  if (!image) console.log(`Missing image for: ${imageUrl}`);
+  if (!image) console.log(`Missing image for the url: ${imageUrl}`);
+});
+
+sections.forEach((section) => {
+  const image = images.find((image) => image.path === section.path);
+  if (!image && !section.text)
+    console.log(`Missing images and text for the section: ${section.path}`);
 });
