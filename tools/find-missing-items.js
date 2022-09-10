60fb1337b9aea169ad9ff21f32ab3805918b4b71
Max Zinovik <zinovik@gmail.com>
2022-09-10 07:03:42 +0400

    sections fixes

--- tools/find-missing-items.js
+++ tools/find-missing-items.js
@@ -6,16 +6,16 @@ images.forEach((image) => {
   const imageUrl = imageUrls.find((imageUrl) =>
     imageUrl.includes(image.filename)
   );
-  if (!imageUrl) console.log(`Missing image url for image: ${image.filename}`);
+  if (!imageUrl) console.log(`! Missing image url for image: ${image.filename}`);
 
   const section = sections.find((section) => section.path === image.path);
   if (!section)
-    console.log(`Missing section for the image path: ${image.path}`);
+    console.log(`! Missing section for the image path: ${image.path}`);
 });
 
 imageUrls.forEach((imageUrl) => {
   const image = images.find((image) => imageUrl.includes(image.filename));
-  if (!image) console.log(`Missing image for the url: ${imageUrl}`);
+  if (!image) console.log(`! Missing image for the url: ${imageUrl}`);
 });
 
 sections.forEach((section) => {
